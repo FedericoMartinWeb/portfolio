2e5a9bc348b90c40dc383e9b5e8c50200909061c
Federico <federicompalacios@hotmail.com>
2017-12-17 15:09:02 -0300

    All Structure

---
+++ app/index.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Tree</title>
+</head>
+<style>
+    body{
+        overflow-x: hidden;
+    }
+    img{
+        height: 100vh;
+        position: absolute;
+        right: -300px;
+        top: 0;
+    }
+</style>
+<body>
+    <img src="Detailed-Tree-Silhouette.svg" alt="tree">
+    <h1>Hola</h1>
+</body>
+</html>
